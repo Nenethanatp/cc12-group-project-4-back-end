9cecfd9fd836cd27f9e1133599d4654ef7c01e19
Thanatporn Lawsirirattana <nthalaw21@gmail.com>
2022-10-14 14:33:11 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port} \n\n\n\n`);
+});
