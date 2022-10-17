a6a8800bc111454936e40b8d971249890beeba4a
wssiripong <w.ssiripong1@gmail.com>
2022-10-17 17:36:49 +0700

    feat:register unrevised

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const authRoute = require('./routes/authRoute');
 const notFound = require('./middlewares/notFound');
 const error = require('./middlewares/error');
 
