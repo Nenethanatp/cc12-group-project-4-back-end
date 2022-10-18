6570bf6082457d5f2da302ed02c3b12328d5b3d3
wssiripong <w.ssiripong1@gmail.com>
2022-10-18 17:12:37 +0700

    feat:getMe-login

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -5,6 +5,7 @@ const authenticate = require('../middlewares/authenticate');
 const router = express.Router();
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 router.get('/me', authenticate, authController.getMe);
 
 module.exports = router;
