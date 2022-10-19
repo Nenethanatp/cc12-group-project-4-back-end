a16d7d069a270f2e262075f792d19d381d666a57
rachataphongl <rachataphong.hi@gmail.com>
2022-10-19 17:12:09 +0700

    feat: like-comment

--- src/config/config.js
+++ src/config/config.js
@@ -6,20 +6,20 @@ module.exports = {
     password: DATABASE_PASSWORD,
     database: 'database_cc12_group4',
     host: '127.0.0.1',
-    dialect: 'mysql',
+    dialect: 'mysql'
   },
   test: {
     username: 'root',
     password: null,
     database: 'database_test',
     host: '127.0.0.1',
-    dialect: 'mysql',
+    dialect: 'mysql'
   },
   production: {
     username: 'root',
     password: null,
     database: 'database_production',
     host: '127.0.0.1',
-    dialect: 'mysql',
-  },
+    dialect: 'mysql'
+  }
 };
--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const postController = require("../controllers/postController");
+const postController = require('../controllers/postController');
 const upload = require('../middlewares/upload');
-const authenticate = require('../middlewares/authenticate')
+const authenticate = require('../middlewares/authenticate');
+const likeController = require('../controllers/likeController');
+const commentController = require('../controllers/commentController');
 
 const router = express.Router();
 
@@ -9,13 +11,19 @@ const router = express.Router();
 
 // router.get('/:id', postController.getById);
 
-router.post("/",
-    authenticate,
-    upload.fields([
-        { name: 'postImage' },
-    ]),
-    postController.createPost);
+router.post(
+  '/',
+  authenticate,
+  upload.fields([{ name: 'postImage' }]),
+  postController.createPost
+);
 
-
+router.post('/:id/likes', authenticate, likeController.toggleLike);
+router.post(
+  '/:id/comments',
+  authenticate,
+  upload.single('commentImage'),
+  commentController.createComment
+);
 
 module.exports = router;
