a69b1dc86d6a47950629c70a5f08069f6e423c1c
KaewSathita <kaew.gothai@gmail.com>
2022-10-20 16:58:55 +0700

    add updatePost

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -62,20 +62,9 @@ exports.createPost = async (req, res, next) => {
       }
     }
 
-    const post = await Post.findOne({
-      where: {
-        id: newPost.id,
-      },
-      include: [
-        { model: User, attributes: { exclude: ['password', 'googleId'] } },
-        PostImage,
-        Location,
-        Type,
-      ],
-      transaction: t,
-    });
-
     await t.commit();
+
+    const post = await getPostbyId(newPost.id);
 
     res.status(200).json({ post });
   } catch (err) {
@@ -105,6 +94,67 @@ exports.getById = async (req, res, next) => {
     }
     res.status(200).json({ post });
   } catch (err) {
+    next(err);
+  }
+};
+
+exports.updatePost = async (req, res, next) => {
+  let t;
+  try {
+    t = await sequelize.transaction();
+    const { id } = req.params;
+    const { content, typeId, latitude, longitude } = req.body;
+
+    if (!content || !content.trim()) {
+      throw new AppError('content is required', 400);
+    }
+    if (!typeId) {
+      throw new AppError('type is required', 400);
+    }
+    if (!latitude || !longitude) {
+      throw new AppError('location is required', 400);
+    }
+
+    const post = await getPostbyId(Number(id));
+    if (!post) {
+      throw new AppError('Post not found.', 404);
+    }   
+
+    post.content = content; 
+    post.typeId = typeId;
+
+    const isThereLocation = await Location.findOne({
+      where: { latitude, longitude },
+    });
+    if (isThereLocation) {
+      post.locationId = isThereLocation.id;
+    } else {
+      const newLocation = await Location.create(
+        { latitude, longitude },
+        { transaction: t }
+      );
+      post.locationId = newLocation.id;
+    }
+
+    await post.save({ transaction: t });
+
+    if (req.files?.postImage) {
+      for (let el of req.files.postImage) {
+        const url = await cloudinary.upload(el.path);
+
+        await PostImage.create(
+          { imageUrl: url, postId: post.id },
+          { transaction: t }
+        );
+        fs.unlinkSync(el.path);
+      }
+    }
+
+    await t.commit();
+
+    res.status(200).json({ post });
+  } catch (err) {
+    await t.rollback();
     next(err);
   }
 };
